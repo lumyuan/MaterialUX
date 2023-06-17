fc131a0dbf12c644c550fa3b6e503bad5a799f7e
莨圆 <2205903933@qq.com>
2023-06-17 08:16:43 +0800

    1.0.13.1

--- app/src/main/java/io/github/lumyuan/ux/MainActivity.java
+++ app/src/main/java/io/github/lumyuan/ux/MainActivity.java
@@ -4,10 +4,14 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.DecelerateInterpolator;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.gyf.immersionbar.ImmersionBar;
 
 import java.util.ArrayList;
@@ -15,6 +19,8 @@ import java.util.ArrayList;
 import io.github.lumyuan.ux.bottomnavigationview.widget.BottomNavigationView;
 import io.github.lumyuan.ux.cleverseekbar.widget.CleverSeekBar;
 import io.github.lumyuan.ux.cleverseekbar.widget.CleverSeekBars;
+import io.github.lumyuan.ux.core.ui.adapter.FastRecyclerViewAdapter;
+import io.github.lumyuan.ux.core.ui.adapter.ViewAdapters;
 import io.github.lumyuan.ux.databinding.ActivityMainBinding;
 import io.github.lumyuan.ux.topbar.widget.TopBar;
 import io.github.lumyuan.ux.ui.PagerAdapterForFragment;
@@ -128,6 +134,21 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "当前位置：" + (position + 1), Toast.LENGTH_SHORT).show();
             return null;
         }));
+
+        RecyclerView recyclerView = new RecyclerView(this);
+
+
+        //View 版本
+        FastRecyclerViewAdapter<String> adapter = new FastRecyclerViewAdapter<>(R.layout.item_basic, new ViewAdapters.OnBindViewHolderListener<String>() {
+            @Override
+            public void onBindViewHolder(FastRecyclerViewAdapter<String> adapter, @NonNull View rootView, String data, int position) {
+                //当列表项显示时调用，一般是用来绑定数据的
+                TextView textView = rootView.findViewById(R.id.text1);
+                textView.setText(data);
+            }
+        });
+
+        recyclerView.setAdapter(adapter);
     }
 
     @Override
